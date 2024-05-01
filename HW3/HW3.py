4e35ae27e3e6829e7c9bf1ef5e201a0755d31b48
WongPeiFong <161627250+WongPeiFong@users.noreply.github.com>
2024-05-01 23:55:50 +0300

    Update HW3.py

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -3,8 +3,10 @@ class IncreaseSpeedIterator:
     def __init__(self, car_instance, max_speed):
         self.car_instance = car_instance
         self.max_speed = max_speed
+
     def __iter__(self):
         return self
+
     def __next__(self):
         if self.car_instance.speed < self.max_speed:
             self.car_instance.speed += 1
@@ -14,8 +16,10 @@ class IncreaseSpeedIterator:
 class DecreaseSpeedIterator:
     def __init__(self, car_instance):
         self.car_instance = car_instance
+
     def __iter__(self):
         return self
+
     def __next__(self):
         if self.car_instance.speed > 0:
             self.car_instance.speed -= 1
@@ -24,17 +28,26 @@ class DecreaseSpeedIterator:
             raise StopIteration
 class Car:
     num_cars_on_road = 0
+
     def __init__(self, speed=0, on_road=False):
         self.speed = speed
         self.on_road = on_road
         if self.on_road:
             Car.num_cars_on_road += 1
     def accelerate(self, amount):
-        self.speed += amount
+        increase_iterator = IncreaseSpeedIterator(self, self.get_speed_limit())
+        for _ in range(amount):
+            try:
+                next(increase_iterator)
+            except StopIteration:
+                break
     def brake(self, amount):
-        self.speed -= amount
-        if self.speed < 0:
-            self.speed = 0
+        decrease_iterator = DecreaseSpeedIterator(self)
+        for _ in range(amount):
+            try:
+                next(decrease_iterator)
+            except StopIteration:
+                break
     @classmethod
     def get_speed_limit(cls):
         return 60
@@ -42,18 +55,23 @@ class Car:
     def honk():
         print("Honk honk!")
     @staticmethod
-    def get_weather():
-        response = requests.get('https://api.openmeteo.org')
-        if response.status_code == 200:
-            data = response.json()
-            return data['weather']
-        else:
+    def show_weather():
+        try:
+            response = requests.get('https://api.openmeteo.org')
+            if response.status_code == 200:
+                data = response.json()
+                return data['weather']
+            else:
+                return "Unknown"
+        except Exception as e:
+            print(f"Error fetching weather: {e}")
             return "Unknown"
     @classmethod
     def get_num_cars_on_road(cls):
         return cls.num_cars_on_road
-    @staticmethod
-    def go_to_parking():
-        Car.num_cars_on_road -= 1
+    def go_to_parking(self):
+        if self.on_road:
+            Car.num_cars_on_road -= 1
+            self.on_road = False
     def __str__(self):
         return f"Car(speed={self.speed}, on_road={self.on_road})"
